8815d2370d09060550a94b9c3c4baa7850c4245f
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-03 17:18:16 +0200

    add data bindings using INotifyPropertyChanged

--- WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/MainWindow.xaml.cs
@@ -1,37 +1,63 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace WPFTutorial
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         bool running = false;
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
-
-            
         }
 
-    //    private void btnToggle_Click(object sender, RoutedEventArgs e)
-    //    {
-    //        if (running)
-    //        {
-    //            // stop
-    //            tbStatus.Text = "Stopped";
-    //            btnToggle.Content = "Run";
-    //        }
-    //        else
-    //        {
-    //            // run
-    //            tbStatus.Text = "Running";
-    //            btnToggle.Content = "Stop";
-    //        }
+        private string boundText;
 
-    //        running = !running; 
-            
-    //    }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set 
+            { 
+                boundText = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+                OnPropertyChanged();
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Set From Code";
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //    private void btnToggle_Click(object sender, RoutedEventArgs e)
+        //    {
+        //        if (running)
+        //        {
+        //            // stop
+        //            tbStatus.Text = "Stopped";
+        //            btnToggle.Content = "Run";
+        //        }
+        //        else
+        //        {
+        //            // run
+        //            tbStatus.Text = "Running";
+        //            btnToggle.Content = "Stop";
+        //        }
+
+        //        running = !running; 
+
+        //    }
     }
 }
