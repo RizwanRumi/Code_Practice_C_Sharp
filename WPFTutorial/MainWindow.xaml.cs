fc167336c81aa1d8160d54107518a54cb92004e3
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-03 09:28:01 +0200

    app WPFTutorial assembly

---
+++ WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace WPFTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+    //    private void btnToggle_Click(object sender, RoutedEventArgs e)
+    //    {
+    //        if (running)
+    //        {
+    //            // stop
+    //            tbStatus.Text = "Stopped";
+    //            btnToggle.Content = "Run";
+    //        }
+    //        else
+    //        {
+    //            // run
+    //            tbStatus.Text = "Running";
+    //            btnToggle.Content = "Stop";
+    //        }
+
+    //        running = !running; 
+            
+    //    }
+    }
+}
