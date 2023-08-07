fee337336d0356b0cc3ec1b7538a28342413a4c1
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-07 22:04:33 +0200

    Add MainWindwViewModel class in ViewModel for Datacontext binding

---
+++ WPFTutorial/MVVMTutorial/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MVVMTutorial.Model;
+using MVVMTutorial.MVVM;
+using System.Collections.ObjectModel;
+
+namespace MVVMTutorial.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item> Items { get; set; }
+        public MainWindowViewModel()
+        {
+
+        }
+
+        private Item selectedItem;
+        public Item SelectedItem
+        {
+            get { return selectedItem; }
+            set 
+            { 
+                selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+       
+    }
+}
