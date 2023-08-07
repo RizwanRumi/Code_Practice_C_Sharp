a965417bc53248718ff87be686cfb445e53442fb
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-07 22:22:14 +0200

    add list of Item in minwindow viewmodel class

--- WPFTutorial/MVVMTutorial/ViewModel/MainWindowViewModel.cs
+++ WPFTutorial/MVVMTutorial/ViewModel/MainWindowViewModel.cs
@@ -9,7 +9,26 @@ namespace MVVMTutorial.ViewModel
         public ObservableCollection<Item> Items { get; set; }
         public MainWindowViewModel()
         {
+            Items = new ObservableCollection<Item>();
+            Items.Add(new Item
+            {
+                Name = "Property1",
+                SerialNumber = "0001",
+                Quantity = 5
+            });
 
+            Items.Add(new Item
+            {
+                Name = "Property2",
+                SerialNumber = "0002",
+                Quantity = 6
+            });
+            Items.Add(new Item
+            {
+                Name = "Property3",
+                SerialNumber = "0003",
+                Quantity = 7
+            });
         }
 
         private Item selectedItem;
