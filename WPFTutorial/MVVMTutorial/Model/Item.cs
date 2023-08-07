c10f88c5d574ea41fb22ab232fb92cfb2e0637e6
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-07 22:03:31 +0200

    add Item class in Model

---
+++ WPFTutorial/MVVMTutorial/Model/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace MVVMTutorial.Model
+{
+    internal class Item
+    {
+        public string Name { get; set; }
+        public string   SerialNumber { get; set; }
+        public int Quality { get; set; }
+    }
+}
