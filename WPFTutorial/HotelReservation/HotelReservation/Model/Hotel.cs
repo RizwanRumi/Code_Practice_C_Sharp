8b180c9e1a985eb4db0c1969ef665369229fdc3c
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-08 18:03:22 +0200

    add hotel class

---
+++ WPFTutorial/HotelReservation/HotelReservation/Model/Hotel.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelReservation.Model
+{
+    public class Hotel
+    {
+        private readonly ReservationBook reservationBook;
+
+        public string Name { get; }
+
+        public Hotel(string name)
+        {
+            Name = name;
+            reservationBook = new ReservationBook();
+        }
+    }
+}
