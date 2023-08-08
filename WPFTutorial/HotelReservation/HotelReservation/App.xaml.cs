812ad3bda89943bbfef2e5a01460e996dc8f4b1c
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-08 19:32:49 +0200

    add overriding OnStartup method in  app class

--- WPFTutorial/HotelReservation/HotelReservation/App.xaml.cs
+++ WPFTutorial/HotelReservation/HotelReservation/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HotelReservation.Exceptions;
+using HotelReservation.Model;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,33 @@ namespace HotelReservation
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("Ocean Life");
+            try
+            {                
+
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 3),
+                    "User 1",
+                    new DateTime(2000, 1, 1),
+                    new DateTime(2000, 1, 2)));
+
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 1),
+                    "User 1",
+                    new DateTime(2000, 1, 1),
+                    new DateTime(2000, 1, 4)));
+            }
+            catch (ReservationConflictException ex)
+            {
+
+            }
+            
+
+            IEnumerable<Reservation> reservations = hotel.GetReservationsForUser("User 1");
+
+            base.OnStartup(e);
+        }
     }
 }
