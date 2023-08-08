0ea803eb848456656c37fdc467301c142cb50b6a
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-08 19:30:52 +0200

    add overriding method for equal and not equal operator

--- WPFTutorial/HotelReservation/HotelReservation/Model/RoomID.cs
+++ WPFTutorial/HotelReservation/HotelReservation/Model/RoomID.cs
@@ -28,5 +28,20 @@ namespace HotelReservation.Model
         {
             return $"{FloorNumber}{RoomNumber}";
         }
+
+        public static bool operator ==(RoomID roomID1, RoomID roomID2)
+        {
+            if(roomID1 is null && roomID2 is null)
+            {
+                return true;
+            }
+
+            return !(roomID1 is null) && roomID1.Equals(roomID2);
+        }
+
+        public static bool operator !=(RoomID roomID1, RoomID roomID2)
+        {
+            return !(roomID1 == roomID2);
+        }
     }
 }
