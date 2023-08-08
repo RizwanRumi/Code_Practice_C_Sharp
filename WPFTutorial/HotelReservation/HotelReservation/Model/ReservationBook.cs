c29fb671c71f29f6cf2587564c1633d44f5d2bcd
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-08 18:04:37 +0200

    add reservationbook class

---
+++ WPFTutorial/HotelReservation/HotelReservation/Model/ReservationBook.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HotelReservation.Model
+{
+    public class ReservationBook
+    {
+        private readonly List<Reservation> reservations;
+        public ReservationBook()
+        {
+            reservations = new List<Reservation>();
+        }
+
+        public IEnumerable<Reservation> GetReservationsForUser(string username)
+        {
+            return reservations.Where(r => r.UserName == username);
+        }
+    }
+}
