9b72a8932485858b1bf7f597f5cded9785e736a4
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-08 18:05:07 +0200

    add roomid class

---
+++ WPFTutorial/HotelReservation/HotelReservation/Model/RoomID.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HotelReservation.Model
+{
+    public class RoomID
+    {
+        public int FloorNumber { get; }
+        public int RoomNumber { get; }
+
+        public RoomID(int floorNumber, int roomnumber)
+        {
+            FloorNumber = floorNumber;
+            RoomNumber = roomnumber;
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj is RoomID roomID &&
+                FloorNumber == roomID.FloorNumber &&
+                RoomNumber == roomID.RoomNumber;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FloorNumber, RoomNumber);
+        }
+
+        public override string ToString()
+        {
+            return $"{FloorNumber}{RoomNumber}";
+        }
+    }
+}
