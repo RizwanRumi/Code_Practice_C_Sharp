25c107e93cc96da7bb4be7b2d1cd03df26a4edc7
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-08 18:04:54 +0200

    add reservation class

---
+++ WPFTutorial/HotelReservation/HotelReservation/Model/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HotelReservation.Model
+{
+    public class Reservation
+    {
+        public RoomID RoomID { get; }
+        public string UserName { get; }
+        public DateTime StartTime { get; }
+        public DateTime EndTime { get; }
+
+        public TimeSpan Length => EndTime.Subtract(StartTime);
+
+        public Reservation(RoomID roomID, string userName, DateTime startTime, DateTime endTime) 
+        {
+            RoomID = roomID;
+            UserName = userName;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+    }
+}
