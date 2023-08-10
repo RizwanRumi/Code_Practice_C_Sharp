f96b4ff6125b4486c699cdccd87618318851db83
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-10 11:38:28 +0200

    add ReservationViewModel class

---
+++ WPFTutorial/HotelReservation/HotelReservation/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using HotelReservation.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.ViewModels
+{
+    public class ReservationViewModel : ViewModelBase
+    {
+        private readonly Reservation _reservation;
+        public string RoomID => _reservation.RoomID? .ToString();
+        public string UserName => _reservation.UserName;
+        public DateTime StartTime => _reservation.StartTime;
+        public DateTime EndTime => _reservation.EndTime;
+
+        public ReservationViewModel(Reservation reservation)
+        {
+            _reservation = reservation;
+        }
+    }
+}
