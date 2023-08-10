96cbeba4644de2f12f0b2ffbcfc54bbbd1a7d14e
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-10 11:14:44 +0200

    add MakeReservationViewModel class

---
+++ WPFTutorial/HotelReservation/HotelReservation/ViewModels/MakeReservationViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HotelReservation.ViewModels
+{
+    public class MakeReservationViewModel : ViewModelBase
+    {
+        private string _username;
+        public string Username 
+        {
+            get 
+            {
+                return _username;
+            }
+            set
+            { 
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            } 
+        }
+
+        private int _floorNumber;
+        public int FloorNumber
+        {
+            get
+            {
+                return _floorNumber;
+            }
+            set
+            {
+                _floorNumber = value;
+                OnPropertyChanged(nameof(FloorNumber));
+            }
+        }
+
+        private int _roomNumber;
+        public int RoomNumber
+        {
+            get
+            {
+                return _roomNumber;
+            }
+            set
+            {
+                _roomNumber = value;
+                OnPropertyChanged(nameof(RoomNumber));
+            }
+        }
+
+        private DateTime _startDate;
+        public DateTime StartDate
+        {
+            get
+            {
+                return _startDate;
+            }
+            set
+            {
+                _startDate = value; OnPropertyChanged(nameof(StartDate));
+            }
+        }
+
+        public ICommand SubmitCommand { get; }
+        public ICommand CancelCommand { get; }
+    }
+}
