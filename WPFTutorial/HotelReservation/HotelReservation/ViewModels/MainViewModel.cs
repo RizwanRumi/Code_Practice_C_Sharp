dc1e9a382e120436169d126c0d073d87171bce32
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-10 13:16:57 +0200

    add MainViewModel class

---
+++ WPFTutorial/HotelReservation/HotelReservation/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentViewModel { get; }
+
+        public MainViewModel()
+        {
+            //CurrentViewModel = new MakeReservationViewModel();
+            CurrentViewModel = new ReservationListingViewModel();
+        }
+    }
+}
