f09ad15e0e20de98857d7c54deebf48a0cf5dd6a
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-10 11:40:18 +0200

    add ReservationListingViewModel class

---
+++ WPFTutorial/HotelReservation/HotelReservation/ViewModels/ReservationListingViewModel.cs
@@ -0,0 +1,28 @@
+﻿using HotelReservation.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HotelReservation.ViewModels
+{
+    public class ReservationListingViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<ReservationViewModel> _reservations;
+
+        public IEnumerable<ReservationViewModel> ReservationList => _reservations;
+
+        public ICommand MakeReservationCommand { get; }
+        public ReservationListingViewModel() 
+        {
+            _reservations = new ObservableCollection<ReservationViewModel>();
+
+            _reservations.Add(new ReservationViewModel(new Reservation(new RoomID(1,2), "User 1", DateTime.Now, DateTime.Now)));
+            _reservations.Add(new ReservationViewModel(new Reservation(new RoomID(3,2), "User 1", DateTime.Now, DateTime.Now)));
+            _reservations.Add(new ReservationViewModel(new Reservation(new RoomID(5,1), "User 1", DateTime.Now, DateTime.Now)));
+        }
+    }
+}
