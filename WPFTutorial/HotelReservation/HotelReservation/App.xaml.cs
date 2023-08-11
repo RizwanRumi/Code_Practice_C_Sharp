6b47bd98d4dbc2b46cd4598107b9f9942d11a9c2
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-11 17:28:31 +0200

    Edit onstartup method

--- WPFTutorial/HotelReservation/HotelReservation/App.xaml.cs
+++ WPFTutorial/HotelReservation/HotelReservation/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using HotelReservation.Exceptions;
 using HotelReservation.Model;
+using HotelReservation.Stores;
 using HotelReservation.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -17,15 +18,19 @@ namespace HotelReservation
     public partial class App : Application
     {
         private readonly Hotel hotel;
+        private readonly NavigationStore navigationStore;
         public App()
         {
             hotel = new Hotel("Ocean Sky");
+            navigationStore = new NavigationStore();
         }
         protected override void OnStartup(StartupEventArgs e)
         {
+            navigationStore.CurrentViewModel = new ReservationListingViewModel();
+
             MainWindow = new MainWindow() 
             {
-                DataContext = new MainViewModel(hotel)
+                DataContext = new MainViewModel(navigationStore)
             };
 
             MainWindow.Show(); 
