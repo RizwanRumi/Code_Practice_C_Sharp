962a08a19afc6a06bbdc14b69fb9e296de2d357d
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-11 17:58:24 +0200

    add OnCurrentViewModelChanged event subscribed in  MainViewModel class

--- WPFTutorial/HotelReservation/HotelReservation/ViewModels/MainViewModel.cs
+++ WPFTutorial/HotelReservation/HotelReservation/ViewModels/MainViewModel.cs
@@ -17,6 +17,12 @@ namespace HotelReservation.ViewModels
         public MainViewModel(NavigationStore navigationStore)
         {
             _navigationStore = navigationStore;
+            _navigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
         }
     }
 }
