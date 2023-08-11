37ca4e93654df1d877a4b51fe169b2e519f29e13
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-11 18:24:43 +0200

    edit NavigateCommand class

--- WPFTutorial/HotelReservation/HotelReservation/Commands/NavigateCommand.cs
+++ WPFTutorial/HotelReservation/HotelReservation/Commands/NavigateCommand.cs
@@ -1,20 +1,23 @@
 ﻿using HotelReservation.Stores;
 using HotelReservation.ViewModels;
+using System;
 
 namespace HotelReservation.Commands
 {
     public class NavigateCommand : CommandBase
     {
         private readonly NavigationStore _navigationStore;
+        private readonly Func<ViewModelBase> _createViewModel;
         
-        public NavigateCommand(NavigationStore navigationStore) 
+        public NavigateCommand(NavigationStore navigationStore, Func<ViewModelBase> createViewModel) 
         {
             _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
         }
         
         public override void Execute(object parameter)
         {
-            _navigationStore.CurrentViewModel = new MakeReservationViewModel(new Model.Hotel(""));            
+            _navigationStore.CurrentViewModel = _createViewModel();            
         }
     }
 }
