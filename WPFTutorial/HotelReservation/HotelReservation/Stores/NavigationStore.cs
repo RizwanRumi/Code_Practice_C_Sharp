f70b6d997112e5527f46649122cf2d02286b6311
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-11 17:26:49 +0200

    add navigationstore class

---
+++ WPFTutorial/HotelReservation/HotelReservation/Stores/NavigationStore.cs
@@ -0,0 +1,25 @@
+﻿using HotelReservation.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservation.Stores
+{
+    public class NavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel 
+        { 
+            get 
+            {
+                return _currentViewModel; 
+            }
+            set 
+            { 
+                _currentViewModel = value; 
+            }
+        }
+    }
+}
