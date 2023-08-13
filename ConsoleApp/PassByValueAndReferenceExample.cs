0c63923ba7aaab4c6f5ebb132e224a463b347164
RizwanRumi <rizwan.cse33@gmail.com>
2023-08-13 13:24:57 +0200

    add Pass by value and reference example

---
+++ ConsoleApp/PassByValueAndReferenceExample.cs
@@ -0,0 +1,39 @@
+﻿internal class PassByValueAndReferenceExample
+{
+    private static void Main(string[] args)
+    {
+        int x = 15;
+        int y = 20;
+
+        Console.WriteLine("Pass by value output:"); 
+        Console.WriteLine(x + " , "+ y);
+        PassByValueExample(x,y);
+        Console.WriteLine(x + " , " + y);
+
+        // Pass by value output: 
+        // 15 , 20
+        // 15 , 20
+
+
+        Console.WriteLine("\nPass by reference output:");
+        Console.WriteLine(x + " , " + y);
+        PassByReferenceExample(ref x, ref y);
+        Console.WriteLine(x + " , " + y);
+
+        // Pass by Reference Output;
+        // 15 , 20
+        // 100, 200
+    }
+
+    private static void PassByValueExample(int x, int y)
+    {
+        x = 100;
+        y = 200;
+    }
+
+    private static void PassByReferenceExample(ref int x, ref int y)
+    {
+        x = 100;
+        y = 200;
+    }
+}
