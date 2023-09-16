7be7bf8bc6ff2d2ed3dcf033bff26879705dec4e
RizwanRumi <rizwan.cse33@gmail.com>
2023-09-16 16:07:41 +0200

    add bootstrap in cshtml pages and use addtaghelpers for routing example

---
+++ EmployeeManagement/EmployeeManagement/Controllers/DepartmentsController.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeManagement.Controllers
+{
+    public class DepartmentsController
+    {
+        public string List()
+        {
+            return "List of Departments";
+        }
+        public string Details()
+        {
+            return "Details of Departments";
+        }
+    }
+}
