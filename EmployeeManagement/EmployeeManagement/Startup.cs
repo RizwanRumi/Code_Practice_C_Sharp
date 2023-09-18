3ecf8e26de0a5ebe321796778f8802e99f072c08
RizwanRumi <rizwan.cse33@gmail.com>
2023-09-18 19:26:37 +0200

    update services in startup file

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -31,7 +31,8 @@ namespace EmployeeManagement
                 options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
 
             services.AddMvc(options => options.EnableEndpointRouting = false);
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            //services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddScoped<IEmployeeRepository, SqlEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
