8b0dc39c9460aeeb8a1730ec9c6f831d3f96e108
RizwanRumi <rizwan.cse33@gmail.com>
2023-09-18 13:16:18 +0200

    add create employee example

--- EmployeeManagement/EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -1,10 +1,18 @@
-﻿namespace EmployeeManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name Cannot Exceed 50 Characters")]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage ="Invalid Email Format")]
+        [Display(Name = "Office Email")]
         public string Email { get; set; }
-        public Dept Department { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
     }
 }
--- EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -17,6 +17,14 @@ namespace EmployeeManagement.Models
             }; 
         }
 
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(x => x.Id) + 1;
+            _employeeList.Add(employee);
+
+            return employee;
+        }
+
         public IEnumerable<Employee> GetAllEmployee()
         {
             return _employeeList;
