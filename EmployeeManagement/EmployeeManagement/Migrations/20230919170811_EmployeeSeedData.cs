19649c3b8d504f3b03a20828a557e222868d9eb6
RizwanRumi <rizwan.cse33@gmail.com>
2023-09-19 19:18:10 +0200

    add seed data in ModelBuilderExtensions class

---
+++ EmployeeManagement/EmployeeManagement/Migrations/20230919170811_EmployeeSeedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class EmployeeSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 1, 2, "rizwan@gmail.com", "Rizwan" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 2, "rumi@gmail.com", "Rumi" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Rizwan",
+                    Department = Dept.IT,
+                    Email = "rizwan@gmail.com"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Rumi",
+                    Department = Dept.IT,
+                    Email = "rumi@gmail.com"
+                }
+            );
+        }
+    }
+}
