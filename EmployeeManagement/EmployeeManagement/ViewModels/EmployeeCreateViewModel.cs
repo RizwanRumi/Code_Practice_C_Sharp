237f0c76e307f145f5b974099d0e441ad4d7bc5c
RizwanRumi <rizwan.cse33@gmail.com>
2023-09-19 23:27:31 +0200

    add EmployeeCreateViewModel class and update Home.cshtml view file

---
+++ EmployeeManagement/EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {        
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name Cannot Exceed 50 Characters")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Invalid Email Format")]
+        [Display(Name = "Office Email")]
+        public string Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
