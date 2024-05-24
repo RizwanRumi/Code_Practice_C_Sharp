c909dd59f76bfb8df95cf69175903830701e1bc3
DESKTOP-RT8N1K7\Rumi <rizwan.cse33@gmail.com>
2024-05-24 22:55:36 +0200

    add simple example for using async task in a method

---
+++ DelegateAndEvents/Player.cs
@@ -0,0 +1,19 @@
+﻿namespace DelegateAndEvents
+{
+    internal class Player
+    {
+        public int Points { get; set; }
+
+        public async Task AddPoints(int points)
+        {
+            Points += points;
+            Console.WriteLine($"Player earned {points}. Total points: {Points}");
+            await Task.Delay( 1000 );
+
+            if ( Points > 100 ) 
+            {
+                Console.WriteLine("Congratulations to achieve unlocked for earning points");
+            }
+        }
+    }
+}
---
+++ DelegateAndEvents/Program.cs
@@ -0,0 +1,7 @@
+﻿using DelegateAndEvents;
+
+Player player = new Player();
+
+await player.AddPoints(30);
+await player.AddPoints(50);
+await player.AddPoints(70);
