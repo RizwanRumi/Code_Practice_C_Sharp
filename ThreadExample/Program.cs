7f8c629c9d377dcd22636df50bf4875ed4119f35
DESKTOP-RT8N1K7\Rumi <rizwan.cse33@gmail.com>
2024-05-25 15:36:08 +0200

    add static method example of thread

---
+++ ThreadExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+
+//Thread t  = Thread.CurrentThread;
+//t.Name = "MainThred";
+//Console.WriteLine(t.Name);
+
+public class MyThread
+{
+    public static void Thread1()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+public class ThreadExample
+{
+    public static void Main()
+    {
+        Thread t1 = new Thread(new ThreadStart(MyThread.Thread1));
+        Thread t2 = new Thread(new ThreadStart(MyThread.Thread1));
+
+        t1.Start();
+        t2.Start();
+    }
+}
