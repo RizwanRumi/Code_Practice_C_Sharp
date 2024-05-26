cbc62a0045360de522350c1941ba606150510a77
DESKTOP-RT8N1K7\Rumi <rizwan.cse33@gmail.com>
2024-05-26 15:53:54 +0200

    example of thread priority

--- ThreadExample/Program.cs
+++ ThreadExample/Program.cs
@@ -10,22 +10,30 @@ public class MyThread
     //public static void Thread1()
 
     // non static method
-    //public void Thread1()
-    //{
-    //    for (int i = 0; i < 10; i++)
-    //    {
-    //        Console.WriteLine(i);
-    //    }
-    //}
-
     public void Thread1()
     {
-        Console.WriteLine("Task one");
+        lock (this)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine(i);
+            }
+        }
+
+        //Naming thread
+        //Thread t = Thread.CurrentThread;
+        //Console.WriteLine(t.Name + " is running");
     }
-    public void Thread2()
-    {
-        Console.WriteLine("Task two");
-    }
+
+    //public void Thread1()
+    //{
+    //    Console.WriteLine("Task one");
+    //}
+    //public void Thread2()
+    //{
+    //    Console.WriteLine("Task two");
+    //}
 }
 
 public class ThreadExample
@@ -39,9 +47,32 @@ public class ThreadExample
         // for non static method
         MyThread myThread = new MyThread();
         Thread t1 = new Thread(new ThreadStart(myThread.Thread1));
-        Thread t2 = new Thread(new ThreadStart(myThread.Thread2));
+        Thread t2 = new Thread(new ThreadStart(myThread.Thread1));
+        Thread t3 = new Thread(new ThreadStart(myThread.Thread1));
+
+        //t1.Name = "Player 1";
+        //t2.Name = "Player 2";
+        //t3.Name = "Player 3";
+
+        //t3.Priority = ThreadPriority.Highest;
+        //t2.Priority = ThreadPriority.Normal;
+        //t1.Priority = ThreadPriority.Lowest;
 
         t1.Start();
+        //t1.Join();
         t2.Start();
+        t3.Start();
+
+        //try
+        //{
+        //    t1.Abort();
+        //    t2.Abort();
+        //}
+        //catch (ThreadAbortException tae)
+        //{
+        //    Console.WriteLine(tae.ToString());
+        //}
+
+        //Console.WriteLine("End of Main");
     }
 }
